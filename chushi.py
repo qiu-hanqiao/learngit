5fcbbfff4a26948c1506f4d83779009b1bcc2c60
hanqiao <hq309@nyu.edu>
2017-09-05 00:51:01 -0700

    learn vim

--- chushi.py
+++ chushi.py
@@ -1,25 +1,32 @@
 add dongxi
 
-di er hang
+	di er hang
+	.....
+	di san hang
 
-di san hang
-
-che xiao
-
-chongxie
-
-amen
-
-d
-
-jia ssh:w
-
-
+	che xiao
+		fadsf
+		fadsf
+	fadsf
+fadsf
+p
+....cfdsafdhotngxie
+sfads
+amten
+dsfadsfa
+cd
+p
+dsfasasfsadsfa
 zhege bucuo
-test branch
+test ranch
 
+dsfadsfdsfadsfdsfadsfa
+dsfadsfdsfadsfdsfadsfa
+dsfadsfdsfadsfdsfadsfa
+dsfadsfdsfadsfdsfadsfa
+dsfadsfdsfadsfdsfadsfa
+dsfadsfdsfadsfdsfadsfa
 push test to master
 
-
 xiu gai test branch
-xindongxi
+gindongxi
